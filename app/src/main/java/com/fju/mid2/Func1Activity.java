755d8cfdb50fb50b76a09871a8b2462cb7a71240
anny1002 <anny1002wade@gmail.com>
2017-12-05 12:02:22 +0800

    number

--- app/src/main/java/com/fju/mid2/Func1Activity.java
+++ app/src/main/java/com/fju/mid2/Func1Activity.java
@@ -1,13 +1,22 @@
 package com.fju.mid2;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Func1Activity extends AppCompatActivity {
-
+    int n = (int) (Math.random()*6+1);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_func1);
+    }
+    public void number(View view){
+        new AlertDialog.Builder(this)
+                .setMessage(n)
+                .setTitle("")
+                .setPositiveButton("OK",null)
+                .show();
     }
 }
